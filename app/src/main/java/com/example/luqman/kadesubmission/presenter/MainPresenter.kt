aa65067bae79e1a270088eaee492eac1fd1a5696
Luqman Sungkar <loekmansungkar@gmail.com>
2018-11-27 06:25:53 +0700

    implement idling resource

--- app/src/main/java/com/example/luqman/kadesubmission/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/luqman/kadesubmission/presenter/MainPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.luqman.kadesubmission.presenter
 
+import android.support.test.espresso.idling.CountingIdlingResource
 import com.example.luqman.kadesubmission.api.ApiRepository
 import com.example.luqman.kadesubmission.api.TheSportDBApi
 import com.example.luqman.kadesubmission.view.MainView
@@ -7,34 +8,50 @@ import com.example.luqman.kadesubmission.model.MatchResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
 
 class MainPresenter(
     private val view: MainView,
     private val apiRepository: ApiRepository,
     private val gson: Gson
 ){
+    private var idlingResource: CountingIdlingResource
+
+    init {
+        idlingResource = CountingIdlingResource("ServerCalls")
+    }
+
     fun getPastMatchList(leagueId: String?){
+        idlingResource.increment()
         view.showLoading()
         val url: String = TheSportDBApi.getPastMatches(leagueId)
 
-        GlobalScope.launch(Dispatchers.Main){
+        val call = GlobalScope.async(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), MatchResponse::class.java)
 
             view.hideLoading()
             view.showMatchList(data.events)
         }
+
+        if(call.isCompleted){
+            idlingResource.decrement()
+        }
     }
 
     fun getNextMatchList(leagueId: String?){
+        idlingResource.increment()
         view.showLoading()
         val url: String = TheSportDBApi.getNextMatches(leagueId)
 
-        GlobalScope.launch(Dispatchers.Main){
+        val call = GlobalScope.async(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), MatchResponse::class.java)
 
             view.hideLoading()
             view.showMatchList(data.events)
+        }
+
+        if(call.isCompleted){
+            idlingResource.decrement()
         }
     }
 }
--- app/src/main/java/com/example/luqman/kadesubmission/presenter/MatchDetailPresenter.kt
+++ app/src/main/java/com/example/luqman/kadesubmission/presenter/MatchDetailPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.luqman.kadesubmission.presenter
 
+import android.support.test.espresso.idling.CountingIdlingResource
 import android.widget.ImageView
 import com.example.luqman.kadesubmission.api.ApiRepository
 import com.example.luqman.kadesubmission.api.TheSportDBApi
@@ -9,34 +10,50 @@ import com.example.luqman.kadesubmission.model.TeamResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
 
 class MatchDetailPresenter(
     private val view: DetailView,
     private val apiRepository: ApiRepository,
     private val gson: Gson
 ){
+    private var idlingResource: CountingIdlingResource
+
+    init {
+        idlingResource = CountingIdlingResource("ServerCalls")
+    }
+
     fun getTeamDetails(teamId: String?, imageView: ImageView){
+        idlingResource.increment()
         view.showLoading()
         var url: String = TheSportDBApi.getTeamDetails(teamId)
 
-        GlobalScope.launch(Dispatchers.Main){
+        val call = GlobalScope.async(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), TeamResponse::class.java)
 
             view.hideLoading()
             view.showTeamBadge(data.teams[0], imageView)
         }
+
+        if (call.isCompleted){
+            idlingResource.decrement()
+        }
     }
 
     fun getMatchDetails(eventId: String?){
+        idlingResource.increment()
         view.showLoading()
         var url: String = TheSportDBApi.getMatchDetails(eventId)
 
-        GlobalScope.launch(Dispatchers.Main){
+        val call = GlobalScope.async(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), MatchResponse::class.java)
 
             view.hideLoading()
             view.showMatchDetail(data.events[0])
+        }
+
+        if(call.isCompleted){
+            idlingResource.decrement()
         }
     }
 }
