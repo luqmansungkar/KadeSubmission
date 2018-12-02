9a22ed833dfa8f504da7d5f10bfd5e323c3b1ec3
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-02 13:02:44 +0700

    change name and add function to get league list

--- app/src/main/java/com/example/luqman/kadesubmission/presenter/MatchPresenter.kt
+++ app/src/main/java/com/example/luqman/kadesubmission/presenter/MatchPresenter.kt
@@ -2,26 +2,40 @@ package com.example.luqman.kadesubmission.presenter
 
 import com.example.luqman.kadesubmission.api.ApiRepository
 import com.example.luqman.kadesubmission.api.TheSportDBApi
-import com.example.luqman.kadesubmission.view.MainView
+import com.example.luqman.kadesubmission.model.LeagueResponse
+import com.example.luqman.kadesubmission.view.MatchView
 import com.example.luqman.kadesubmission.model.MatchResponse
 import com.example.luqman.kadesubmission.util.EspressoIdlingResource
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
-class MainPresenter(
-    private val view: MainView,
+class MatchPresenter(
+    private val view: MatchView,
     private val apiRepository: ApiRepository,
     private val gson: Gson
 ){
+
+    fun getLeagueList(){
+        EspressoIdlingResource.increment()
+        view.showLoading()
+        val url: String = TheSportDBApi.getListAllLeagues()
+
+        GlobalScope.launch(Dispatchers.Main){
+            val data = gson.fromJson(apiRepository.doRequest(url).await(), LeagueResponse::class.java)
+
+            view.hideLoading()
+            view.showLeagueList(data.countrys)
+        }
+    }
 
     fun getPastMatchList(leagueId: String?){
         EspressoIdlingResource.increment()
         view.showLoading()
         val url: String = TheSportDBApi.getPastMatches(leagueId)
 
-        val call = GlobalScope.async(Dispatchers.Main){
+        GlobalScope.launch(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), MatchResponse::class.java)
 
             view.hideLoading()
@@ -35,7 +49,7 @@ class MainPresenter(
         view.showLoading()
         val url: String = TheSportDBApi.getNextMatches(leagueId)
 
-        val call = GlobalScope.async(Dispatchers.Main){
+        GlobalScope.launch(Dispatchers.Main){
             val data = gson.fromJson(apiRepository.doRequest(url).await(), MatchResponse::class.java)
 
             view.hideLoading()
