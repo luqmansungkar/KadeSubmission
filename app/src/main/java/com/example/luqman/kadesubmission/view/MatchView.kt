6624ca3c3e9c9a63dd7c527e8256f56ecf68dbdc
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-02 13:01:45 +0700

    change name and add function to show league list

--- app/src/main/java/com/example/luqman/kadesubmission/view/MatchView.kt
+++ app/src/main/java/com/example/luqman/kadesubmission/view/MatchView.kt
@@ -1,9 +1,11 @@
 package com.example.luqman.kadesubmission.view
 
 import com.example.luqman.kadesubmission.model.Event
+import com.example.luqman.kadesubmission.model.Leagues
 
-interface MainView{
+interface MatchView{
     fun showLoading()
     fun hideLoading()
+    fun showLeagueList(leagues: List<Leagues>)
     fun showMatchList(data: List<Event>)
 }
