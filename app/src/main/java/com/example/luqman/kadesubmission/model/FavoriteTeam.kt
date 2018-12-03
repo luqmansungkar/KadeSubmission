0093213d2f49e4b5111198ac7c40c56779280e3c
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-03 12:48:01 +0700

    add fav team db model

---
+++ app/src/main/java/com/example/luqman/kadesubmission/model/FavoriteTeam.kt
@@ -0,0 +1,16 @@
+package com.example.luqman.kadesubmission.model
+
+data class FavoriteTeam(
+    val id: Long?,
+    val teamId: String?,
+    val teamBadge: String?,
+    val teamName: String?
+){
+    companion object {
+        const val FAVORITE_TEAM_TABLE: String = "FAVORITE_TEAM_TABLE"
+        const val ID: String = "ID_"
+        const val TEAM_ID: String = "TEAM_ID"
+        const val TEAM_BADGE: String = "TEAM_BADGE"
+        const val TEAM_NAME: String = "TEAM_NAME"
+    }
+}
