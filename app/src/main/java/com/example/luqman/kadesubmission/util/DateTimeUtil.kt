592244c74fb52d3eb0352a761b14088365ef2a28
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-12 19:22:45 +0700

    prevent force close when time format not match

--- app/src/main/java/com/example/luqman/kadesubmission/util/DateTimeUtil.kt
+++ app/src/main/java/com/example/luqman/kadesubmission/util/DateTimeUtil.kt
@@ -1,15 +1,21 @@
 package com.example.luqman.kadesubmission.util
 
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 
 object DateTimeUtil{
     fun formatDateTime(dateTime: String?, fromFormat: String, toFormat: String): String{
-        var fromSimpleDateFormat = SimpleDateFormat(fromFormat, Locale.getDefault())
+        return try{
+            val fromSimpleDateFormat = SimpleDateFormat(fromFormat, Locale.getDefault())
 
-        val oldDateTime = fromSimpleDateFormat.parse(dateTime)
-        val toSimpleDateFormat = SimpleDateFormat(toFormat, Locale.getDefault())
+            val oldDateTime = fromSimpleDateFormat.parse(dateTime)
+            val toSimpleDateFormat = SimpleDateFormat(toFormat, Locale.getDefault())
 
-        return toSimpleDateFormat.format(oldDateTime)
+            toSimpleDateFormat.format(oldDateTime)
+        }catch (e: Exception){
+            dateTime ?: ""
+        }
+
     }
 }
